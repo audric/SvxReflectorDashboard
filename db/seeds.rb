568f59f4ef0de11f7ac1bc21e634fce9ea98c7af
Audric <github@audric.it>
2026-03-04 16:51:01 +0100

    Set seed admin as approved so login works immediately

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 if User.count.zero?
-  admin = User.new(callsign: "ADM1N", password: "changeme", password_confirmation: "changeme", role: "admin", reflector_admin: true)
+  admin = User.new(callsign: "ADM1N", password: "changeme", password_confirmation: "changeme", role: "admin", reflector_admin: true, approved: true)
   admin.save!
   puts "Created default admin user: ADM1N / changeme"
 end
