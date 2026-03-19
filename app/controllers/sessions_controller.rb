74549a9e584f704501d2f849bd9ab5d346ec9b41
Audric <github@audric.it>
2026-03-19 15:05:15 +0100

    Remove 'Logged out' flash message on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,6 +24,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
-    redirect_to root_path, notice: "Logged out"
+    redirect_to login_path
   end
 end
